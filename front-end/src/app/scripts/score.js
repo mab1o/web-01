c4b221daff0ffcd25c969a06d8f93f79efa57e5d
Margaux Blondel <margaux.blondel@free.fr>
2024-03-05 10:28:31 +0100

    refactor(front-end): modify ES5 prototype for ES6 class in score.js

--- front-end/src/app/scripts/score.js
+++ front-end/src/app/scripts/score.js
@@ -1,21 +1,21 @@
 import template from "../views/score.html";
 import { parseUrl } from "./utils";
 
-  // TODO #class: use the ES6 class keyword
-  /* class ScoreComponent constructor */
-  export function ScoreComponent() {
-    // TODO #extends: call super(template)
-    var params = parseUrl();
-    this.template = template;
-    this.name = params.name;
-    this.size = parseInt(params.size);
-    this.time = parseInt(params.time);
-  }
+  export class ScoreComponent{
+      /* class ScoreComponent constructor */
+      constructor() {
+        // TODO #extends: call super(template)
+        var params = parseUrl();
+        this.template = template;
+        this.name = params.name;
+        this.size = parseInt(params.size);
+        this.time = parseInt(params.time);
+      };
 
-  // TODO #class: turn function into a method of ScoreComponent
-  /* method ScoreComponent.init */
-  ScoreComponent.prototype.init = function init() {
-    document.getElementById("name").innerText = this.name;
-    document.getElementById("size").innerText = this.size;
-    document.getElementById("time").innerText = this.time;
-  };
+      /* method ScoreComponent.init */
+      init() {
+          document.getElementById("name").innerText = this.name;
+          document.getElementById("size").innerText = this.size;
+          document.getElementById("time").innerText = this.time;
+      };
+  }
