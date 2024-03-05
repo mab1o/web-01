6d3c5c3d39e71735d3e3a14650a896bef6854486
Margaux Blondel <margaux.blondel@free.fr>
2024-03-05 10:23:20 +0100

    refactor(front-end): modify ES5 prototype for ES6 class in welcome.js

--- front-end/src/app/scripts/welcome.js
+++ front-end/src/app/scripts/welcome.js
@@ -1,41 +1,41 @@
 import template from "../views/welcome.html";
+  export class  WelcomeComponent{
 
-  // TODO #class: use the ES6 class keyword
-  /* class WelcomeComponent constructor  */
-  export function WelcomeComponent() {
-    // TODO #extends: call super(template)
-    this.template = template;
-  }
+    /* class WelcomeComponent constructor  */
+    constructor(){
+        // TODO #extends: call super(template)
+        this.template = template;
+    };
 
-  // TODO #class: turn function into a method of WelcomeComponent
-  /* method WelcomeComponent.init */
-  WelcomeComponent.prototype.init = function init() {
-    var form = document.querySelector("form.form-signin");
+    /* method WelcomeComponent.init */
+    init() {
+        var form = document.querySelector("form.form-signin");
 
-    form.addEventListener(
-      "submit",
-      // TODO #arrow-function: use arrow function instead.
-      function (event) {
-        event.preventDefault();
-        if (form.checkValidity() === false) {
-          event.stopPropagation();
-          form.classList.add("was-validated");
-        } else {
-          var name = event.srcElement.querySelector("#nickname").value;
-          var size = parseInt(event.srcElement.querySelector("#size").value);
+        form.addEventListener(
+          "submit",
+          // TODO #arrow-function: use arrow function instead.
+          function (event) {
+            event.preventDefault();
+            if (form.checkValidity() === false) {
+              event.stopPropagation();
+              form.classList.add("was-validated");
+            } else {
+              var name = event.srcElement.querySelector("#nickname").value;
+              var size = parseInt(event.srcElement.querySelector("#size").value);
 
-          _startGame(name, size);
-        }
-      }.bind(this),
-      false
-    );
+              this._startGame(name, size);
+            }
+          }.bind(this),
+          false
+        );
 
-    return this;
-  };
+        return this;
+    };
 
-  // TODO #class: turn function into a method of WelcomeComponent
-  function _startGame(name, size) {
-    var gamePage = "./#game";
-    // TODO #template-literals:  use template literals (backquotes)
-    window.location = gamePage + "?name=" + name + "&size=" + size;
+    _startGame(name, size) {
+        var gamePage = "./#game";
+        // TODO #template-literals:  use template literals (backquotes)
+        window.location = gamePage + "?name=" + name + "&size=" + size;
+    };
+
   }
