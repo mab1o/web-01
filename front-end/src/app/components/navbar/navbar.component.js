a12468ee15896d0360a2cefda3ca8e6f0356493c
Margaux Blondel <margaux.blondel@free.fr>
2024-03-06 16:48:01 +0100

    refactor(front-end): move Navbar component to its own file

---
+++ front-end/src/app/components/navbar/navbar.component.js
@@ -0,0 +1,9 @@
+import template from "./navbar.component.html";
+import "./navbar.component.css";
+
+export class NavbarComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = template;
+    }
+}
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,9 +3,11 @@ import { WelcomeComponent } from "./app/components/welcome/welcome.component";
 import { GameComponent } from "./app/components/game/game.component";
 import { ScoreComponent } from "./app/components/score/score.component";
 // TODO #import-css: use ES side-effect imports to import styles/style.css
-
+import { NavbarComponent } from "./app/components/navbar/navbar.component";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./app/styles/style.css";
+
+customElements.define("my-navbar", NavbarComponent);
 
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
