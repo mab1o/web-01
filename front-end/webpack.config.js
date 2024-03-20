76973718bd8dd09edca6d07b25ff4f8b9d4a45e7
Margaux Blondel <margaux.blondel@free.fr>
2024-03-20 09:30:42 +0100

    feat(front-end): ensure compatibility with old browsers

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -32,6 +32,16 @@
           test: /\.(png|jpg|gif)$/i,
           type: 'asset/resource'
         },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
